433f834b38bc43279e4209f4862ad67678bcb1cd
Hozumi Kaneko <h.kaneko@reasonwhy.jp>
2016-03-30 11:51:24 +0900

    add ecs-cli

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,15 @@ JQ="jq --raw-output --exit-status"
 check_ecs_cli() {
     which ecs-cli | cat
     ecs-cli configure -c circlecitest -r ap-northeast-1 | cat
-    cat pwd
-    cat CIRCLE_SHA1
+    docker images | cat
+    pwd | cat
+    echo CIRCLE_SHA1
     cat ~/docker-compose.yml
     cat ~/.ecs/config
+    docker build -t rw/circleci .
+    aws ecr get-login --region us-east-1 | bash
+    docker tag rw/circleci 418299443513.dkr.ecr.us-east-1.amazonaws.com/rw/circleci:$CIRCLE_SHA1
+    docker push 418299443513.dkr.ecr.us-east-1.amazonaws.com/rw/circleci:$CIRCLE_SHA1
     ecs-cli compose up
 }
 
