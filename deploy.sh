d8ec924cd4428382d8eb8d33deeb9c8def2b395d
Hozumi Kaneko <h.kaneko@reasonwhy.jp>
2016-03-30 14:31:27 +0900

    add ecs-cli

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,9 @@ check_ecs_cli() {
     aws ecr get-login --region us-east-1 | bash
     docker push 418299443513.dkr.ecr.us-east-1.amazonaws.com/rw/circleci:$CIRCLE_SHA1
     cat ./docker-compose.yml
+    sed -i -e "s/\${CIRCLE_SHA1}/$CIRCLE_SHA1/" docker-compose.yml
+    cat ./docker-compose.yml    
     ecs-cli compose up
 }
 
 check_ecs_cli
-
